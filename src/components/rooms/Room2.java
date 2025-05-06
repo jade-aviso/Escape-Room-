9fbdfc5cb941f5419d622fdd249ebaae4d27c34e
Jade Sapphire D. Aviso <147261623+jade-aviso@users.noreply.github.com>
2025-05-06 21:48:01 +0000

    room 2 chances works

--- src/components/rooms/Room2.java
+++ src/components/rooms/Room2.java
@@ -5,22 +5,47 @@ import components.core.Puzzle;
 public class Room2 extends Puzzle {
     @Override
     public boolean play() {
+        int chances = 3;
+        String hintAnswer;
         System.out.println("Welcome to Room 2!");
         System.out.println("Guess the 3-digit code: Hint – it's 123 reversed.");
-        String code = scanner.nextLine().trim();
+        
 
-        if (code.equals("321")) {
-            System.out.println("Finally, you made it passed the first round.");
-            return true;
-        } else {
-            System.out.println("Surely you can do better than that.");
+        while(chances != 0){
+            System.out.print("Your answer: ");
+            String code = scanner.nextLine().trim();
+
+            if (code.equals("321")) {
+                System.out.println("Finally, you made it passed the first round.");
+                return true;
+            } 
+            else {
+                chances--;
+                if (chances > 0) {
+                    System.out.println("Try again! Chances left: " + chances);
+
+                    
+                        //if it is first time losing
+                            if(chances == 2){
+                                //want a hint?
+                                System.out.println("Would you like a hint? Y/N");
+                                //scanner
+                                hintAnswer = scanner.nextLine();
+                                //if yes
+                                if(hintAnswer.equals("Y")){System.out.println("this is hint");}
+                            
+                            }
+                        }
+            }
+        
+        }
+        System.out.println("Surely you can do better than that.");
             System.out.println("__   _____  _   _   ____ ___ _____ ____  _ ");
             System.out.println("\\ \\ / / _ \\| | | | |  _ \\_ _| ____|  _ \\| |");
             System.out.println(" \\ V / | | | | | | | | | | ||  _| | | | | |");
             System.out.println("  | || |_| | |_| | | |_| | || |___| |_| |_|");
             System.out.println("  |_| \\___/ \\___/  |____/___|_____|____/(_)");
             return false;
-        }
     }
 }
 
